294a6241d731337324431540fa6a589eaeb2517c
espoirferide <espoirferide@gmail.com>
2026-04-03 09:32:31 +0100

    project starting

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+   
+       <h1 className="text-3xl font-bold text-purple-600">Hello, Vite!</h1>      
+            
+  )
+}
+
+export default App
